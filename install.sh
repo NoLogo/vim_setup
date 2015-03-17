097cea13ef011573f90edc296f1f9510e2087670
Philip Gardner <philip@no-logo.co.uk>
2015-03-17 10:28:03 +0000

    added pathogen & an install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#run this from the top directory
+git submodule update
+
+mkdir ~/.vim ~/.vim/autoload
+
+ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $(pwd)/pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
+ln -s $(pwd)/bundle ~/.vim/bundle
+
